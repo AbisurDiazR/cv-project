c3ecb943961da3379b8e6fad2114d8236550a4c3
AbisurDiazR <abisurdiazramirez@gmail.com>
2020-12-22 02:54:06 -0600

    Creación de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CvComponent } from './components/cv/cv.component';
+import { HomeComponent } from './components/home/home.component';
+import { RecomendacionComponent } from './components/recomendacion/recomendacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'cv',
+    component: CvComponent
+  },
+  {
+    path: 'recomendacion',
+    component: RecomendacionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CvComponent } from './components/cv/cv.component';
+import { RecomendacionComponent } from './components/recomendacion/recomendacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CvComponent,
+    RecomendacionComponent
   ],
   imports: [
     BrowserModule,
